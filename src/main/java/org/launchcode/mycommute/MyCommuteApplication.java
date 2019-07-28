361cb9abc705982306f7885c2ce203ed7ef69e9b
Denise <ddplocher@gmail.com>
2019-07-27 19:41:39 -0500

    Home page set up

---
+++ src/main/java/org/launchcode/mycommute/MyCommuteApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.mycommute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCommuteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCommuteApplication.class, args);
+	}
+
+}
