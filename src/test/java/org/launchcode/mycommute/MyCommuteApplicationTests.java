d071c166b6302adc4770b8ab04ecd189b0f3507f
Denise <ddplocher@gmail.com>
2019-08-06 19:35:39 -0500

    Home page complete with bootstrap

---
+++ src/test/java/org/launchcode/mycommute/MyCommuteApplicationTests.java
@@ -0,0 +1,16 @@
+package org.launchcode.mycommute;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyCommuteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
